9343c6fa697b655f9ffc1a7a8eebf13eba0b9ede
Oscarwasoccupied <42104387+Oscarwasoccupied@users.noreply.github.com>
2022-12-03 23:25:01 -0500

    add model.py
    use simply CNN

---
+++ model/model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class CNNNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1=nn.Sequential(
+            nn.Conv2d(in_channels=1,out_channels=16,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2=nn.Sequential(
+            nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3=nn.Sequential(
+            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4=nn.Sequential(
+            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.flatten=nn.Flatten()
+        self.linear1=nn.Linear(in_features=128*5*4,out_features=128)
+        self.linear2=nn.Linear(in_features=128,out_features=1)
+        self.output=nn.Sigmoid()
+    
+    def forward(self,input_data):
+        # add one dimension
+        input_data.unsqueeze_(1)
+        x=self.conv1(input_data)
+        x=self.conv2(x)
+        x=self.conv3(x)
+        x=self.conv4(x)
+        x=self.flatten(x)
+        x=self.linear1(x)
+        logits=self.linear2(x)
+        output=self.output(logits)
+        
+        return output
